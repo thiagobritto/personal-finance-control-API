53859c4293aac4b4781f13df1a68e07895244d49
thiagobritto <thiagobritto.developer@gmail.com>
2026-02-02 16:45:11 -0300

    feat: initialize spring boot finance project

---
+++ finance/src/main/java/com/thiagobritto/finance/FinanceApplication.java
@@ -0,0 +1,14 @@
+package com.thiagobritto.finance;
+
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FinanceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FinanceApplication.class, args);
+	}
+
+}
